3569c1bfe90854f11afed02b061e00fb4ad882a6
ShuaiB-s-git-hub <101641458+ShuaiB-s-git-hub@users.noreply.github.com>
2022-10-16 20:51:41 +0800

    Add files via upload

---
+++ 1.3 字符编码.py
@@ -0,0 +1,7 @@
+# 工程创建人：袁某
+# 创作时间： 2022/10/15 20:14
+
+print (chr (0b100111001011000))#中文“乘”的编码 （unicode编码）
+print (ord ('乘'))             #化为10进制
+
+#实际上python的字符编码有一定的问题，具体见 https://www.cnblogs.com/huxi/archive/2010/12/05/1897271.html
